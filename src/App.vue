3eb33be8a3a24c35f518a54dad3bb631b406c228
eriawan <eriawan@gti.co.id>
2021-07-05 07:58:53 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,84 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <c-header />
+
+    <c-side-bar />
+
+    <v-main>
+      <v-slide-x-transition mode="out-in">
+        <router-view></router-view>
+      </v-slide-x-transition>
+    </v-main>
+
+    <c-footer />
+
+    <keep-alive>
+      <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+        transition="dialogbottom-transition"
+      >
+        <component :is="currentComponent"></component>
+      </v-dialog>
+    </keep-alive>
+
+    <c-alert />
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<style type="text/css">
+.v-toolbar {
+  flex: 0 !important;
+}
+.v-application .py-3 {
+  text-align: center !important;
+}
+.v-card__text {
+  text-align: center !important;
+}
 </style>
+
+<script>
+import { mapActions, mapGetters } from "vuex"
+
+import CHeader from "@/components/CHeader.vue"
+import CFooter from "@/components/CFooter.vue"
+import CSideBar from "@/components/CSideBar.vue"
+import CAlert from "@/components/CAlert.vue"
+import Search from "@/views/Search.vue"
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+
+export default {
+  name: "App",
+  components: {
+    CHeader,
+    CFooter,
+    CSideBar,
+    CAlert,
+    Search,
+    Login,
+    Register,
+  },
+  methods: {
+    ...mapActions({
+      setStatusDialog: "dialog/setStatus",
+    }),
+  },
+  computed: {
+    ...mapGetters({
+      statusDialog: "dialog/status",
+      currentComponent: "dialog/component",
+    }),
+    dialog: {
+      get() {
+        return this.statusDialog
+      },
+      set(value) {
+        this.setStatusDialog(value)
+      },
+    },
+  },
+}
+</script>
